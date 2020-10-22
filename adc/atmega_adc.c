823d2be18e5da354667ccaf1aa2c786f86a69656
igr-bel <30845578+igr-bel@users.noreply.github.com>
2020-10-22 11:41:10 +0200

    Update atmega_adc.c

--- adc/atmega_adc.c
+++ adc/atmega_adc.c
@@ -13,16 +13,16 @@ void atmega_adc_init()
 	//clock divider = 64 --> fadc = 125 kHz
 	
 	ADCSRA = 0xC6;		//	|ADEN|ADSC|ADFR|ADIF|ADIE|ADPS2|ADPS1|ADPS0|
-						//	| 1  | 1  | 0  | 0  | 0  |  1  |  1  |  0  |
+				//	| 1  | 1  | 0  | 0  | 0  |  1  |  1  |  0  |
 }//atmega_adc_init
 
 //--------------------------------------------------------------------------------------------------------------------------
 unsigned int adc_get_value(unsigned char channel)
 {
-	ADMUX = ( channel | (ADMUX & 0xF0) );	// REFS1:REFS0	0:0	/AREF
-											// ADLAR(bit 5) 0	/ADCH <- 2 high bit
+	ADMUX = ( channel | (ADMUX & 0xF0) );		// REFS1:REFS0	0:0	/AREF
+							// ADLAR(bit 5) 0	/ADCH <- 2 high bit
 	_delay_us(15);
-	ADCSRA |= 0x40;							// ADSC = 1
+	ADCSRA |= 0x40;					// ADSC = 1
 	while ( 0 == (ADCSRA & 0x10) );			// while (ADIF == 0)
 	return ( (unsigned int) (ADCL + (ADCH<<8)) );
 	
